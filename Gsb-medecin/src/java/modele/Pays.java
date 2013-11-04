5fcda3e7070b35500138f92324d529e6f3c178a7
elo.logiudice <elo.logiudice@gmail.com>
2013-11-04 11:53:15 +0000

    creation du modele des classes à terminer

---
+++ Gsb-medecin/src/java/modele/Pays.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author elo
+ */
+public class Pays {
+    private Collection<Dep> lesDeps;
+
+    public Pays() {
+    }
+
+    public Collection<Dep> getLesDeps() {
+        return lesDeps;
+    }
+    
+    
+}
