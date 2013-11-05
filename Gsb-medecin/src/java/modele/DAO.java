f14a6d342869572fbf0b40e96ef985973084368a
elo.logiudice <elo.logiudice@gmail.com>
2013-11-05 14:54:42 +0000

    DAO terminer

--- Gsb-medecin/src/java/modele/DAO.java
+++ Gsb-medecin/src/java/modele/DAO.java
@@ -6,45 +6,48 @@ package modele;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author elo
  */
 public class DAO {
-    static void charge(Pays d) {
+
+    static Collection<Dep> getLesDeps() {
+
+        Collection<Dep> lesDeps = new HashSet<Dep>();
+
         try {
             Connection con = Connect.get();
             Statement req;
             req = con.createStatement();
-            ResultSet rs = req.executeQuery("select * from ");
-            rs.next();
-            d.setNom(rs.getString("nom"));
-            d.setLieu(rs.getString("lieu"));
-            d.setDate(rs.getDate("dateb"));
+            ResultSet rs = req.executeQuery("Select distinct departement from medecin;");
 
-
-            Statement reqT = con.createStatement();
-            ResultSet rsT = reqT.executeQuery("select * from type");
-
-            while (rsT.next()) {
-                Statement reqE = con.createStatement();
-                ResultSet rsE = reqE.executeQuery("select * from emplacement where idtype="
-                                                    + rsT.getString("id")  );
-                Collection<Emplacement> colEmp = new HashSet<Emplacement>();
-                while (rsE.next()) {
-                    Emplacement e = new Emplacement(rsE.getString("code"),rsE.getString("nomab"));
-                    colEmp.add(e);
+            while (rs.next()) {
+                Collection<Med> lesMeds = new HashSet<Med>();
+                Statement req2;
+                req2 = con.createStatement();
+                ResultSet rs2 = req2.executeQuery("select * from medecin where departement =" + rs.getString("departement"));
+                while (rs2.next()) {
+                    Med m = new Med(rs2.getString("nom"), rs2.getString("prenom"), rs2.getString("adresse"), rs2.getString("tel"),
+                            rs2.getString("specialitecomplementaire"), rs2.getString("id"));
+                    lesMeds.add(m);
                 }
-                
-                Type t = new Type(rsT.getString("id"),rsT.getString("description"),rsT.getDouble( "prix"),colEmp);
-                b.addType(t);
-                
-            }
+                Dep d = new Dep(rs.getString("departement"), lesMeds);
+                lesDeps.add(d);
 
+            }
         } catch (SQLException ex) {
             Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return lesDeps;
+
+
     }
 }
