16b5ab001229ce74a9bf0c26cdfd52516123fd9f
elo.logiudice@gmail.com <elo.logiudice@gmail.com@250410ce-058d-78ab-9c6c-5f58ed12c7cd>
2013-11-05 14:57:22 +0000

    DAO terminer

---
+++ Gsb-medecin/src/java/modele/Test.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author elo
+ */
+public class Test {
+    public static void main(String[] args) {
+        Pays p = new Pays();
+        System.out.println("");
+    }
+}
