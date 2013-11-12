7927460c9f075508f1e431e41f6c5bbc7733d9e8
elo.logiudice <elo.logiudice@gmail.com>
2013-11-12 14:57:47 +0000

    Controleur à finir

--- Gsb-medecin/src/java/controleur/Control.java
+++ Gsb-medecin/src/java/controleur/Control.java
@@ -4,10 +4,96 @@
  */
 package controleur;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modele.Dep;
+import modele.Med;
+import modele.Pays;
+
 /**
  *
  * @author elo
  */
-public class Control {
-    
+public class Control extends HttpServlet {
+
+    private Pays p;
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String page;
+        String choix;
+        choix = request.getParameter("choix");
+        if (choix != null) {
+            Collection<Med> m = p.getLesDeps(choix).getLesMeds();
+            request.setAttribute("listeMeds",m);
+            page = "listeMeds.jsp";
+        } else {
+            Collection<Dep> d = p.getLesDeps();
+            request.setAttribute("listeDeps", d);
+            page = "listeDeps.jsp";
+        }
+        request.getRequestDispatcher(page).forward(request, response);
+    }
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        p = new Pays(); //instancie les objets utiles
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
